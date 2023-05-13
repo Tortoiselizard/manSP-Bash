ac27e37f27f6646fd6603ec7072ada1c5846281d
tortoiselizard <troy00pernia@gmail.com>
2023-05-13 18:23:26 -0400

    change mansp: change the path of files: comand.json, comand.txt and comando.txt

--- mansp.sh
+++ mansp.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #Crear archivo comand.txt con informacion del manual
-man $1 | col -b | tr -s ' ' | sed 's/\"/\\\"/g' > comand.txt
-sed -i 's/	/ /g' comand.txt
+man $1 | col -b | tr -s ' ' | sed 's/\"/\\\"/g' > /data/data/com.termux/files/home/scripts/mansp/comand.txt
+sed -i 's/	/ /g' /data/data/com.termux/files/home/scripts/mansp/comand.txt
 
 #Contar numero de lineas del archivo comand.txt
-nlineas=$(wc -l < comand.txt | awk '{print $1}')
+nlineas=$(wc -l < /data/data/com.termux/files/home/scripts/mansp/comand.txt | awk '{print $1}')
 
 #Declara arreglo para manipular ibformacion
 lineasTexto=()
@@ -13,30 +13,30 @@ linea=1
 
 while [ $linea -le $nlineas ]
 do
-  lineasTexto+=("$(head -n $linea comand.txt | tail -1)")
+  lineasTexto+=("$(head -n $linea /data/data/com.termux/files/home/scripts/mansp/comand.txt | tail -1)")
   linea=$(($linea+1))
 done
 
 #Crear archivo JSON para hacer solicitud
-echo '{' > comand.json
-echo '  "sourceLanguage": "en",' >> comand.json
-echo '  "text": [' >> comand.json
+echo '{' > /data/data/com.termux/files/home/scripts/mansp/comand.json
+echo '  "sourceLanguage": "en",' >> /data/data/com.termux/files/home/scripts/mansp/comand.json
+echo '  "text": [' >> /data/data/com.termux/files/home/scripts/mansp/comand.json
 for linea in "${lineasTexto[@]}"
 do
   if [[ $linea = ${lineasTexto[-1]} ]]; then
-echo "    \"$linea\"" >> comand.json
+echo "    \"$linea\"" >> /data/data/com.termux/files/home/scripts/mansp/comand.json
   else
-echo "    \"$linea\"," >> comand.json
+echo "    \"$linea\"," >> /data/data/com.termux/files/home/scripts/mansp/comand.json
   fi
 done 
-echo '  ]' >> comand.json
-echo '}' >> comand.json
+echo '  ]' >> /data/data/com.termux/files/home/scripts/mansp/comand.json
+echo '}' >> /data/data/com.termux/files/home/scripts/mansp/comand.json
 
 #Hacer solicitud
-curl -X POST -H "Content-Type: application/json" -d @comand.json https://mansp-back.onrender.com/linux/man > comando.txt
+curl -X POST -H "Content-Type: application/json" -d @/data/data/com.termux/files/home/scripts/mansp/comand.json https://mansp-back.onrender.com/linux/man > /data/data/com.termux/files/home/scripts/mansp/comando.txt
 
 #Leer informacion
-less comando.txt
+less /data/data/com.termux/files/home/scripts/mansp/comando.txt
 
 #solicitud GET
 #respuesta=$(curl -s https://mansp-back.onrender.com/linux)
