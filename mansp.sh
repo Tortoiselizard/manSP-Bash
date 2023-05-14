5fad8fe27df1c12a4bdcfc7c9f0aa98f62c869da
tortoiselizard <troy00pernia@gmail.com>
2023-05-14 14:29:37 -0400

    change mansp.sh: add conditions of errors

--- mansp.sh
+++ mansp.sh
@@ -3,10 +3,22 @@
 #ruta directorio de comandos
 pathComands="/data/data/com.termux/files/home/scripts/mansp/comands"
 
+#Cuando no se pasa argumento
+if [ "$1" = "" ]; then
+	echo "usege: mansp.sh name ..."
+	exit 0
+fi
+
 #Buscar en biblioteca
 if test -e "$pathComands/$1.txt"; then
-less "$pathComands/$1.txt"
-else
+	less "$pathComands/$1.txt"
+	exit 0
+fi
+
+if ! man $1 > "$pathComands/null" 2>&1; then
+	echo "el comando $1 no se encuentra en man"
+	exit 1
+fi
 
 #Crear archivo comand.txt con informacion del manual
 man $1 | col -b | tr -s ' ' | sed 's/\"/\\\"/g' > /data/data/com.termux/files/home/scripts/mansp/comand.txt
@@ -45,8 +57,6 @@ curl -X POST -H "Content-Type: application/json" -d @/data/data/com.termux/files
 
 #Leer informacion
 less "$pathComands/$1.txt"
-
-fi
 
 #solicitud GET
 #respuesta=$(curl -s https://mansp-back.onrender.com/linux)
